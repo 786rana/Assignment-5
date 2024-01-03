7543270b8df020bad71d89a65e5aea6c55378283
Rana Rafaqat <119485229+786rana@users.noreply.github.com>
2024-01-03 17:28:09 +0500

    Add files via upload

---
+++ Assignment 5/Client/Std.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Assignment_5.Client.Pages
+{
+    public class StdModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
